b85b14cadfdb400a35f8a0bfd6ca500888493690
Mugunthan-S <mugunthan.s.work@gmail.com>
2024-05-05 13:36:52 +0530

    Completed TCS question-1

---
+++ TCS/Divisible_9.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class TCS {
+    public static void main(String args[]) {
+        int number = -1;
+        Scanner sc = new Scanner(System.in);
+        do {
+            // Prompting user if they pass inputs other than integers
+            try {
+                System.out.print("Enter a three-digit product code (0 to stop): ");
+                number = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Enter only integers :(");
+                sc.next();
+                continue;
+            }
+            
+            // Checking the range of the input
+            if (number >= 100 && number <= 999) {
+                // Checking divisibilty by 9
+                if (number%9 == 0) System.out.println("Product with code "+ number +" is divisible by 9");
+                else if (number%9 != 0) System.out.println("Product with code "+ number +" is not divisible by 9");
+            }
+            // Prompting user is they pass input out of range
+            else if (number != 0) System.out.println("Product code should be in the range - 100 <= code <= 999");
+        }while (number != 0); // Code exits once the user passes 0 as input
+    } 
+}
